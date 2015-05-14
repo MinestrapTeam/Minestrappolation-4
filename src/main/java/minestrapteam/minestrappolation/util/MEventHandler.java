e74c796e8249a6dd4eebc4e2f70a0089f7b4f13d
Dylan Greer <greerd248@yahoo.com>
2015-05-14 02:55:27 -0400

    Armor effects

--- src/main/java/minestrapteam/minestrappolation/util/MEventHandler.java
+++ src/main/java/minestrapteam/minestrappolation/util/MEventHandler.java
@@ -10,9 +10,14 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.Phase;
+import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -51,6 +56,38 @@ public class MEventHandler {
 			}
 			
 		}	
+	}
+	
+
+	@SubscribeEvent
+	public void playerUpdate(LivingUpdateEvent event)
+	{
+		if(event.entity instanceof EntityPlayer)
+		{
+				EntityPlayer player = (EntityPlayer)event.entity;
+				ItemStack helmet = player.getCurrentArmor(3);
+				ItemStack chest = player.getCurrentArmor(2);
+				ItemStack pants = player.getCurrentArmor(1);
+				ItemStack boots = player.getCurrentArmor(0);
+				
+				if (helmet != null && chest != null && pants != null && boots != null)
+				{
+					if (helmet.getItem() == MItems.meurodite_helmet && chest.getItem() == MItems.meurodite_chestplate && pants.getItem() == MItems.meurodite_leggings && boots.getItem() == MItems.meurodite_boots)
+					{
+						player.addPotionEffect(new PotionEffect(Potion.fireResistance.id, 2, 0, true, false));
+					}
+					else if (helmet.getItem() == MItems.torite_helmet && chest.getItem() == MItems.torite_chestplate && pants.getItem() == MItems.torite_leggings && boots.getItem() == MItems.torite_boots)
+					{
+						player.addPotionEffect(new PotionEffect(Potion.regeneration.id, 2, 0, true, false));
+					}
+					else if (helmet.getItem() == MItems.titanium_helmet && chest.getItem() == MItems.titanium_chestplate && pants.getItem() == MItems.titanium_leggings && boots.getItem() == MItems.titanium_boots)
+					{
+						player.addPotionEffect(new PotionEffect(Potion.resistance.id, 2, 1, true, false));
+					}
+				}
+			
+		}
+		
 	}
 	
 
