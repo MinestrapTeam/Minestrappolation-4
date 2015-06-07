539041ea4375305a7d294791aaa26a84ddb2a7b7
Dylan Greer <greerd248@yahoo.com>
2015-06-07 15:56:23 -0400

    Chance fixes

--- src/main/java/minestrapteam/minestrappolation/util/Tables.java
+++ src/main/java/minestrapteam/minestrappolation/util/Tables.java
@@ -39,50 +39,50 @@ public class Tables {
     	Chance.addToChanceTable("sifter_red_sand", null, 25);
     	Chance.addToChanceTable("sifter_red_sand", new ItemStack(MItems.chunks, 1, 1), 20);
     	Chance.addToChanceTable("sifter_red_sand", new ItemStack(MItems.chunks, 1, 4), 5);
-    	Chance.addToChanceTable("sifter_red_sand", Items.clay_ball, 50);
+    	Chance.addToChanceTable("sifter_red_sand", new ItemStack(Items.clay_ball, 1, 0), 50);
     	Chance.addToChanceTable("sifter_red_sand", new ItemStack(MItems.chunks, 1, 5), 3);
     	//Sifter - Gravel
     	Chance.addToChanceTable("sifter_gravel", null, 25);
     	Chance.addToChanceTable("sifter_gravel", new ItemStack(MItems.chunks, 1, 2), 20);
-    	Chance.addToChanceTable("sifter_gravel", Items.bone, 10);
-    	Chance.addToChanceTable("sifter_gravel", Items.flint, 20);
-    	Chance.addToChanceTable("sifter_gravel", Items.gunpowder, 5);
+    	Chance.addToChanceTable("sifter_gravel", new ItemStack(Items.bone, 1, 0), 10);
+    	Chance.addToChanceTable("sifter_gravel", new ItemStack(Items.flint, 1, 0), 20);
+    	Chance.addToChanceTable("sifter_gravel", new ItemStack(Items.gunpowder, 1, 0), 5);
     	Chance.addToChanceTable("sifter_gravel", new ItemStack(MItems.chunks, 1, 7), 3);
     	//Sifter - Dirt
     	Chance.addToChanceTable("sifter_dirt", null, 25);
     	Chance.addToChanceTable("sifter_dirt", new ItemStack(MItems.chunks, 1, 0), 5);
-    	Chance.addToChanceTable("sifter_dirt", Items.wheat_seeds, 10);
-    	Chance.addToChanceTable("sifter_dirt", Items.potato, 1);
-    	Chance.addToChanceTable("sifter_dirt", Items.carrot, 1);
-    	Chance.addToChanceTable("sifter_dirt", MItems.sunstone_shard, 3);
+    	Chance.addToChanceTable("sifter_dirt", new ItemStack(Items.wheat_seeds, 1, 0), 10);
+    	Chance.addToChanceTable("sifter_dirt", new ItemStack(Items.potato, 1, 0), 1);
+    	Chance.addToChanceTable("sifter_dirt", new ItemStack(Items.carrot, 1, 0), 1);
+    	Chance.addToChanceTable("sifter_dirt", new ItemStack(MItems.sunstone_shard, 1, 0), 3);
     	//Sifter - Mycelium
     	Chance.addToChanceTable("sifter_mycelium", null, 25);
     	Chance.addToChanceTable("sifter_mycelium", new ItemStack(MItems.chunks, 1, 0), 5);
     	Chance.addToChanceTable("sifter_mycelium", new ItemStack(MItems.chunks, 1, 4), 1);
-    	Chance.addToChanceTable("sifter_mycelium", Blocks.red_mushroom, 10);
-    	Chance.addToChanceTable("sifter_mycelium", Blocks.brown_mushroom, 10);
-    	Chance.addToChanceTable("sifter_mycelium", MItems.uranium, 1);
-    	Chance.addToChanceTable("sifter_mycelium", MItems.plutonium, 1);
+    	Chance.addToChanceTable("sifter_mycelium", new ItemStack(Item.getItemFromBlock(Blocks.red_mushroom), 1, 0), 10);
+    	Chance.addToChanceTable("sifter_mycelium", new ItemStack(Item.getItemFromBlock(Blocks.brown_mushroom), 1, 0), 10);
+    	Chance.addToChanceTable("sifter_mycelium", new ItemStack(MItems.uranium, 1, 0), 1);
+    	Chance.addToChanceTable("sifter_mycelium", new ItemStack(MItems.plutonium, 1, 0), 1);
     	//Sifter - Permafrost
     	Chance.addToChanceTable("sifter_permafrost", null, 25);
     	Chance.addToChanceTable("sifter_permafrost", new ItemStack(MItems.chunks, 1, 3), 5);
-    	Chance.addToChanceTable("sifter_permafrost", Items.snowball, 30);
-    	Chance.addToChanceTable("sifter_permafrost", Blocks.ice, 1);
+    	Chance.addToChanceTable("sifter_permafrost", new ItemStack(Items.snowball, 1, 0), 30);
+    	Chance.addToChanceTable("sifter_permafrost", new ItemStack(Item.getItemFromBlock(Blocks.ice), 1, 0), 1);
     	//Sifter - Cold Sand
     	Chance.addToChanceTable("sifter_cold_sand", null, 25);
     	Chance.addToChanceTable("sifter_cold_sand", new ItemStack(MItems.chunks, 1, 4), 20);
     	Chance.addToChanceTable("sifter_cold_sand", new ItemStack(MItems.chunks, 1, 3), 5);
-    	Chance.addToChanceTable("sifter_cold_sand", Items.clay_ball, 20);
-    	Chance.addToChanceTable("sifter_cold_sand", Items.snowball, 30);
-    	Chance.addToChanceTable("sifter_cold_sand", Blocks.ice, 1);
+    	Chance.addToChanceTable("sifter_cold_sand", new ItemStack(Items.clay_ball, 1, 0), 20);
+    	Chance.addToChanceTable("sifter_cold_sand", new ItemStack(Items.snowball, 1, 0), 30);
+    	Chance.addToChanceTable("sifter_cold_sand", new ItemStack(Item.getItemFromBlock(Blocks.ice), 1, 0), 1);
     	//Sifter - Soul Sand
     	Chance.addToChanceTable("sifter_soul_sand", null, 25);
-    	Chance.addToChanceTable("sifter_soul_sand", Items.quartz, 20);
-    	Chance.addToChanceTable("sifter_soul_sand", Items.nether_wart, 1);
-    	Chance.addToChanceTable("sifter_soul_sand", Items.ghast_tear, 1);
-    	Chance.addToChanceTable("sifter_soul_sand", Items.blaze_powder, 10);
-    	Chance.addToChanceTable("sifter_soul_sand", MItems.blaze_shard, 5);
-    	Chance.addToChanceTable("sifter_soul_sand", MItems.soul_gem, 1);
+    	Chance.addToChanceTable("sifter_soul_sand", new ItemStack(Items.quartz, 1, 0), 20);
+    	Chance.addToChanceTable("sifter_soul_sand", new ItemStack(Items.nether_wart, 1, 0), 1);
+    	Chance.addToChanceTable("sifter_soul_sand", new ItemStack(Items.ghast_tear, 1, 0), 1);
+    	Chance.addToChanceTable("sifter_soul_sand", new ItemStack(Items.blaze_powder, 1, 0), 10);
+    	Chance.addToChanceTable("sifter_soul_sand", new ItemStack(MItems.blaze_shard, 1, 0), 5);
+    	Chance.addToChanceTable("sifter_soul_sand", new ItemStack(MItems.soul_gem, 1, 0), 1);
     	Chance.addToChanceTable("sifter_soul_sand", new ItemStack(MItems.chunks, 1, 8), 1);
 	}
 }
