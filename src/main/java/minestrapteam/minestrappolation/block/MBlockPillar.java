ac2cf436a3ccb735ad8d597163059d9a748fc089
Dylan Greer <greerd248@yahoo.com>
2015-06-16 12:36:20 -0400

    Added pillar block

---
+++ src/main/java/minestrapteam/minestrappolation/block/MBlockPillar.java
@@ -0,0 +1,13 @@
+package minestrapteam.minestrappolation.block;
+
+import net.minecraft.block.BlockRotatedPillar;
+import net.minecraft.block.material.Material;
+
+public class MBlockPillar extends BlockRotatedPillar{
+
+	protected MBlockPillar(Material materialIn) 
+	{
+		super(materialIn);
+	}
+
+}
