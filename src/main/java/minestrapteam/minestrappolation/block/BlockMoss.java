470788b27f36b263466f10c446a01b185e089a42
Delocuro <delocuro@gmail.com>
2015-06-18 17:11:59 -0400

    Moss?
    -Added framework for Moss Covering. Not implemented or tested yet, doing
    that later.

---
+++ src/main/java/minestrapteam/minestrappolation/block/BlockMoss.java
@@ -0,0 +1,171 @@
+package minestrapteam.minestrappolation.block;
+
+import java.util.Iterator;
+import java.util.Random;
+
+import minestrapteam.minestrappolation.Minestrappolation;
+import minestrapteam.minestrappolation.lib.MBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockBush;
+import net.minecraft.block.BlockDirt;
+import net.minecraft.block.IGrowable;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.EnumDyeColor;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class BlockMoss extends BlockBush implements IGrowable
+{
+
+	protected BlockMoss() 
+	{
+		super(Material.snow);
+		this.setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 0.0625F, 1.0F);
+        this.setTickRandomly(true);
+        this.setCreativeTab(Minestrappolation.tabMDecor);
+        this.setBlockBoundsFromMeta(0);
+	}
+	
+	public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand)
+    {
+        if (rand.nextInt(25) == 0)
+        {
+            int i = 5;
+            boolean flag = true;
+            Iterator iterator = BlockPos.getAllInBoxMutable(pos.add(-4, -1, -4), pos.add(4, 1, 4)).iterator();
+
+            while (iterator.hasNext())
+            {
+                BlockPos blockpos1 = (BlockPos)iterator.next();
+
+                if (worldIn.getBlockState(blockpos1).getBlock() == this)
+                {
+                    --i;
+
+                    if (i <= 0)
+                    {
+                        return;
+                    }
+                }
+            }
+
+            BlockPos blockpos2 = pos.add(rand.nextInt(3) - 1, rand.nextInt(2) - rand.nextInt(2), rand.nextInt(3) - 1);
+
+            for (int j = 0; j < 4; ++j)
+            {
+                if (worldIn.isAirBlock(blockpos2) && this.canBlockStay(worldIn, blockpos2, this.getDefaultState()))
+                {
+                    pos = blockpos2;
+                }
+
+                blockpos2 = pos.add(rand.nextInt(3) - 1, rand.nextInt(2) - rand.nextInt(2), rand.nextInt(3) - 1);
+            }
+
+            if (worldIn.isAirBlock(blockpos2) && this.canBlockStay(worldIn, blockpos2, this.getDefaultState()))
+            {
+                worldIn.setBlockState(blockpos2, this.getDefaultState(), 2);
+            }
+        }
+    }
+	
+	public boolean canPlaceBlockAt(World worldIn, BlockPos pos)
+    {
+        return super.canPlaceBlockAt(worldIn, pos) && this.canBlockStay(worldIn, pos, this.getDefaultState());
+    }
+	
+	protected boolean canPlaceBlockOn(Block ground)
+    {
+		 return ground == Blocks.grass || ground == Blocks.dirt || ground == Blocks.farmland || ground == Blocks.cobblestone || ground == Blocks.mossy_cobblestone || ground == Blocks.stonebrick || ground == MBlocks.biome_cobble || ground == MBlocks.biome_bricks || ground == MBlocks.mossy || ground == MBlocks.mossy_bricks || ground == MBlocks.cracked_bricks || ground == MBlocks.pattern_bricks || ground == MBlocks.stone_pattern_bricks || ground == MBlocks.chiseled || ground == MBlocks.dirt_permafrost || ground == MBlocks.lichen_permafrost;
+    }
+	
+	public boolean canBlockStay(World worldIn, BlockPos pos, IBlockState state)
+    {
+        if (pos.getY() >= 0 && pos.getY() < 256)
+        {
+            IBlockState iblockstate1 = worldIn.getBlockState(pos.down());
+            return iblockstate1.getBlock() == Blocks.mycelium ? true : (iblockstate1.getBlock() == Blocks.dirt && iblockstate1.getValue(BlockDirt.VARIANT) == BlockDirt.DirtType.PODZOL ? true : (iblockstate1.getBlock() == MBlocks.lichen_permafrost ? true : (iblockstate1.getBlock() == MBlocks.dirt_permafrost ? true : worldIn.getLight(pos) < 13 && iblockstate1.getBlock().canSustainPlant(worldIn, pos.down(), net.minecraft.util.EnumFacing.UP, this))));
+        }
+        else
+        {
+            return false;
+        }
+    }
+	
+	public boolean canGrow(World worldIn, BlockPos pos, IBlockState state, boolean isClient)
+    {
+        return false;
+    }
+
+	public boolean isOpaqueCube()
+    {
+        return false;
+    }
+
+    public boolean isFullCube()
+    {
+        return false;
+    }
+
+    /**
+     * Sets the block's bounds for rendering it as an item
+     */
+    public void setBlockBoundsForItemRender()
+    {
+        this.setBlockBoundsFromMeta(0);
+    }
+
+    public void setBlockBoundsBasedOnState(IBlockAccess worldIn, BlockPos pos)
+    {
+        this.setBlockBoundsFromMeta(0);
+    }
+
+    protected void setBlockBoundsFromMeta(int meta)
+    {
+        byte b0 = 0;
+        float f = (float)(1 * (1 + b0)) / 16.0F;
+        this.setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, f, 1.0F);
+    }
+
+    public void onNeighborBlockChange(World worldIn, BlockPos pos, IBlockState state, Block neighborBlock)
+    {
+        this.checkForDrop(worldIn, pos, state);
+    }
+
+    private boolean checkForDrop(World worldIn, BlockPos pos, IBlockState state)
+    {
+        if (!this.canBlockStay(worldIn, pos, state))
+        {
+            this.dropBlockAsItem(worldIn, pos, state, 0);
+            worldIn.setBlockToAir(pos);
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    @SideOnly(Side.CLIENT)
+    public boolean shouldSideBeRendered(IBlockAccess worldIn, BlockPos pos, EnumFacing side)
+    {
+        return side == EnumFacing.UP ? true : super.shouldSideBeRendered(worldIn, pos, side);
+    }
+
+	@Override
+	public boolean canUseBonemeal(World worldIn, Random rand, BlockPos pos, IBlockState state) 
+	{
+		return false;
+	}
+
+	@Override
+	public void grow(World worldIn, Random rand, BlockPos pos, IBlockState state) 
+	{
+	}
+}
