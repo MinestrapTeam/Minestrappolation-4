46724adbb3ca8186045354d4aadbee730f253348
Dylan Greer <greerd248@yahoo.com>
2015-06-20 01:58:18 -0400

    Fixes magma displacement

--- src/main/java/minestrapteam/minestrappolation/block/BlockMagma.java
+++ src/main/java/minestrapteam/minestrappolation/block/BlockMagma.java
@@ -1,10 +1,14 @@
 package minestrapteam.minestrappolation.block;
 
+import minestrapteam.minestrappolation.enumtypes.MStoneType;
 import minestrapteam.minestrappolation.lib.MBlocks;
 import minestrapteam.minestrappolation.lib.MFluid;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
 import net.minecraftforge.fluids.BlockFluidClassic;
 import net.minecraftforge.fluids.Fluid;
@@ -24,5 +28,51 @@ public class BlockMagma extends BlockFluidClassic
 	{
 		return MFluid.MAGMA;
 	}
+	
+	@Override
+	public void onNeighborBlockChange(World worldIn, BlockPos pos, IBlockState state, Block neighborBlock)
+    {
+        this.checkForMixing(worldIn, pos, state);
+    }
+
+    public boolean checkForMixing(World worldIn, BlockPos pos, IBlockState state)
+    {
+        if (this.blockMaterial == Material.lava)
+        {
+            boolean flag = false;
+            EnumFacing[] aenumfacing = EnumFacing.values();
+            int i = aenumfacing.length;
+
+            for (int j = 0; j < i; ++j)
+            {
+                EnumFacing enumfacing = aenumfacing[j];
+
+                if (enumfacing != EnumFacing.DOWN && worldIn.getBlockState(pos.offset(enumfacing)).getBlock().getMaterial() == Material.water)
+                {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (flag)
+            {
+                Integer integer = (Integer)state.getValue(LEVEL);
+
+                if (integer.intValue() == 0)
+                {
+                    worldIn.setBlockState(pos, MBlocks.biome_stones.getStateFromMeta(MStoneType.DEEPSTONE.getMetadata()));
+                    return true;
+                }
+
+                if (integer.intValue() <= 4)
+                {
+                    worldIn.setBlockState(pos, MBlocks.biome_cobble.getStateFromMeta(MStoneType.DEEPSTONE.getMetadata()));
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
 	
 }
