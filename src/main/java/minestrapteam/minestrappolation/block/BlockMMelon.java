807be764c6726bcef41011793a1c2cbe8f69e224
Delocuro <delocuro@gmail.com>
2015-06-21 01:43:58 -0400

    I don't even anymore.
    -Sometimes you shouldn't add things just because you have textures for
    them.

---
+++ src/main/java/minestrapteam/minestrappolation/block/BlockMMelon.java
@@ -0,0 +1,7 @@
+package minestrapteam.minestrappolation.block;
+
+import net.minecraft.block.BlockMelon;
+
+public class BlockMMelon extends BlockMelon
+{
+}
--- src/main/java/minestrapteam/minestrappolation/crafting/stonecutter/StonecuttingManager.java
+++ src/main/java/minestrapteam/minestrappolation/crafting/stonecutter/StonecuttingManager.java
@@ -83,6 +83,7 @@ public class StonecuttingManager
 		this.addRecipe(new ItemStack(Blocks.brick_block, 1, 0), null, new Object[] { "BB", "BB", 'B', Items.brick});
 		this.addRecipe(new ItemStack(Blocks.nether_brick, 1, 0), null, new Object[] { "BB", "BB", 'B', Items.netherbrick});
 		this.addRecipe(new ItemStack(MBlocks.obsidian_bricks, 1), null, new Object[] { "BB", "BB", 'B', MItems.obsidian_brick});
+		this.addRecipe(new ItemStack(MBlocks.melon_bricks, 1), null, new Object[] { "MMM", "MMM", "MMM", 'M', Items.melon});
 		
 		//Mossy Brick Blocks
 		this.addRecipe(new ItemStack(Blocks.stonebrick, 9, 1), new ItemStack(MBlocks.moss), new Object[] { "BBB", "BBB", "BBB", 'B', new ItemStack(Blocks.stonebrick, 1, 0)});
