bf8daced44ee1158b111dd64f5d1cca8c8194336
Delocuro <delocuro@gmail.com>
2015-07-09 17:29:48 -0400

    Hang Gliders...
    -Attempted (and failed) to add Hang Glider crash damage.

--- src/main/java/minestrapteam/minestrappolation/item/ItemHangGlider.java
+++ src/main/java/minestrapteam/minestrappolation/item/ItemHangGlider.java
@@ -13,92 +13,63 @@ import net.minecraft.world.World;
 
 public class ItemHangGlider extends Item
 {
-	private boolean	initCheck	= true;
-	private boolean	checking	= false;
+	private double prevVelX;
+	private double prevVelZ;
 	
 	public ItemHangGlider()
 	{
 		this.maxStackSize = 1;
         this.setMaxDamage(50);
         this.isDamageable();
+        this.prevVelX = 0;
+        this.prevVelZ = 0;
 	}
 	
 	public void onUpdate(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected)
 	{
 		
-			if(isSelected && entityIn instanceof EntityPlayer)
+		if(isSelected && entityIn instanceof EntityPlayer)
+		{
+			EntityPlayer entityplayer = (EntityPlayer)entityIn;
+			double absX = Math.abs(entityplayer.motionX);
+			double absZ = Math.abs(entityplayer.motionZ);
+			entityplayer.motionY *= 0.6D;
+			entityplayer.fallDistance = 0;
+			
+			for(int i = 5 ; i > 0 ; i--)
 			{
-				EntityPlayer entityplayer = (EntityPlayer)entityIn;
-				double absX = Math.abs(entityplayer.motionX);
-				double absZ = Math.abs(entityplayer.motionZ);
-				entityplayer.motionY *= 0.6D;
-				entityplayer.fallDistance = 0;
-				
-				for(int i = 5 ; i > 0 ; i--)
+				BlockPos pos = new BlockPos(entityplayer.posX, entityplayer.posY - i, entityplayer.posZ);
+				if (worldIn.getBlockState(pos).getBlock() == Blocks.lava || worldIn.getBlockState(pos).getBlock() == Blocks.flowing_lava || worldIn.getBlockState(pos).getBlock() == MBlocks.magma || worldIn.getBlockState(pos).getBlock() == Blocks.fire || worldIn.getBlockState(pos).getBlock() == MBlocks.blazium_block)
 				{
-					BlockPos pos = new BlockPos(entityplayer.posX, entityplayer.posY - i, entityplayer.posZ);
-					if (worldIn.getBlockState(pos).getBlock() == Blocks.lava || worldIn.getBlockState(pos).getBlock() == Blocks.flowing_lava || worldIn.getBlockState(pos).getBlock() == MBlocks.magma)
+					entityplayer.motionY += (5 - i) * 0.01D;
+					if(Chance.rand.nextInt(100) == 1)
 					{
-						entityplayer.motionY += (5 - i) * 0.01D;
-						if(Chance.rand.nextInt(100) == 1)
-						{
-							if(stack.getItemDamage() < stack.getMaxDamage())
-								stack.attemptDamageItem(1, Chance.rand);
-							else
-								entityplayer.destroyCurrentEquippedItem();
-						}
+						if(stack.getItemDamage() < stack.getMaxDamage())
+							stack.attemptDamageItem(1, Chance.rand);
+						else
+							entityplayer.destroyCurrentEquippedItem();
 					}
 				}
-				if (absX < 0.9D)
-				{
-					entityplayer.motionX *= 1.06D;
-				}
-				else if (absX < 0.15D)
-				{
-					entityplayer.motionX *= 1.1D;
-				}
-				
-				if (absZ < 0.9D)
-				{
-					entityplayer.motionZ *= 1.06D;
-				}
-				else if (absZ < 0.15D)
-				{
-					entityplayer.motionZ *= 1.1D;
-				}
-				
-				if (this.initCheck && absX + absZ > 0.15D)
-				{
-					this.checking = true;
-					this.initCheck = false;
-				}
-				else if (!entityplayer.isCollidedHorizontally)
-				{
-					this.checking = false;
-					this.initCheck = true;
-				}
-				
-				// The problem for no damage is that player.isCollidedHorizontally
-				// only
-				// works client side, so it damages only client side, and not server
-				// side. these packets do nothing.
-				if (this.checking && entityIn.isCollidedHorizontally)
-				{
-					entityplayer.motionX = 0D;
-					entityplayer.motionZ = 0D;
-					this.checking = false;
-					this.initCheck = true;
-				}
-				
-				// wall colision
-				/*
-				 * if((Math.abs(prev2XMotion) > 0.3 || Math.abs(prev2ZMotion) > 0.3
-				 * || (Math.abs(prev2XMotion) > 0.3 && Math.abs(prev2ZMotion) >
-				 * 0.3)) && player.isCollidedHorizontally &&
-				 * Math.abs(player.motionY) > 0.13) { par1ItemStack.damageItem(1,
-				 * Minecraft.getMinecraft().thePlayer); }
-				 */
+			}
+			if (absX < 0.9D)
+			{
+				entityplayer.motionX *= 1.06D;
+			}
+			else if (absX < 0.15D)
+			{
+				entityplayer.motionX *= 1.1D;
+			}
 			
+			if (absZ < 0.9D)
+			{
+				entityplayer.motionZ *= 1.06D;
+			}
+			else if (absZ < 0.15D)
+			{
+				entityplayer.motionZ *= 1.1D;
+			}
+			
+			//TODO: Hang Gliders should take damage every time the player runs into a wall at high velocities. So far I have not found a way to make this work, since entityplayer.isCollidedHorizontally appears to be client-side only...
 		}
 	}
 }
