71ef60217e00a0aba677e8cc373cd9f8bc31aef1
Dylan Greer <greerd248@yahoo.com>
2015-07-27 23:53:16 -0400

    Fixes seaweed generation

--- src/main/java/minestrapteam/mods/minestrappolation/world/WorldGenSeaweed.java
+++ src/main/java/minestrapteam/mods/minestrappolation/world/WorldGenSeaweed.java
@@ -14,15 +14,16 @@ public class WorldGenSeaweed extends WorldGenerator
 
 	@Override
 	public boolean generate(World world, Random rand, BlockPos pos) 
-	{
-		BlockPos randPos = pos.add(rand.nextInt(8) - rand.nextInt(8), rand.nextInt(4) - rand.nextInt(4), rand.nextInt(8) - rand.nextInt(8));
-		
-		if (world.getBlockState(randPos.up()).getBlock() == Blocks.water && world.getBlockState(randPos.down()).getBlock() != Blocks.water)
+	{		
+		if (world.getBlockState(pos.up()).getBlock() == Blocks.water && world.getBlockState(pos.down()).getBlock() != Blocks.water)
 		{	
 			for (int i = 0; i < 64; i++)
 			{
-				randPos = pos.add(rand.nextInt(8) - rand.nextInt(8), 0, rand.nextInt(8) - rand.nextInt(8));
-				world.setBlockState(randPos, MBlocks.seaweed.getDefaultState(), 2);
+				BlockPos randPos = pos.add(rand.nextInt(8) - rand.nextInt(8), rand.nextInt(4) - rand.nextInt(4), rand.nextInt(8) - rand.nextInt(8));
+				if(MBlocks.seaweed.canPlaceBlockAt(world, randPos))
+				{
+					world.setBlockState(randPos, MBlocks.seaweed.getDefaultState(), 2);
+				}
 			}
 		}
 		return true;
