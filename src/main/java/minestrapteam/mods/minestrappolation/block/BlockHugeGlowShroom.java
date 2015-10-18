fa6f961ef6709d779e59fd11d5012ca500816430
Delocuro <delocuro@gmail.com>
2015-10-18 13:05:41 -0400

    Glowshroom Assets!
    -Added all missing Glowshroom models and blockstates.
    -Glowshrooms now emit light.
    -Glowshrooms are now semi-transparent.

--- src/main/java/minestrapteam/mods/minestrappolation/block/BlockHugeGlowShroom.java
+++ src/main/java/minestrapteam/mods/minestrappolation/block/BlockHugeGlowShroom.java
@@ -1,8 +1,16 @@
 package minestrapteam.mods.minestrappolation.block;
 
+import minestrapteam.mods.minestrappolation.lib.MBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockHugeMushroom;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumWorldBlockLayer;
+import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockHugeGlowShroom extends BlockHugeMushroom
 {
@@ -12,4 +20,29 @@ public class BlockHugeGlowShroom extends BlockHugeMushroom
 		super(materialIn, smallBlock);
 	}
 	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public EnumWorldBlockLayer getBlockLayer()
+	{
+		return EnumWorldBlockLayer.TRANSLUCENT;
+	}
+	
+	@Override
+	public boolean isOpaqueCube()
+	{
+		return false;
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public boolean shouldSideBeRendered(IBlockAccess worldIn, BlockPos pos, EnumFacing side)
+	{
+		IBlockState iblockstate = worldIn.getBlockState(pos);
+		Block block = iblockstate.getBlock();
+		
+		if (block == this)
+			return false;
+		else
+			return true;
+	}
 }
