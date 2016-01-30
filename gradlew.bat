adaf66c8a46f9b10d7e7a0e10548936b7846bbe4
Domenic Portuesi <dportuesi@gmail.com>
2016-01-30 13:27:20 -0500

    Workspace changes

--- gradlew.bat
+++ gradlew.bat
@@ -11,7 +11,6 @@ if "%OS%"=="Windows_NT" setlocal
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS=
 
-set JAVA_HOME=C:/Program Files/Java/jdk1.8.0_60
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
@@ -33,7 +32,7 @@ echo location of your Java installation.
 goto fail
 
 :findJavaFromJavaHome
-set JAVA_HOME=C:/Program Files/Java/jdk1.8.0_60
+set JAVA_HOME=%JAVA_HOME:"=%
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
 if exist "%JAVA_EXE%" goto init
