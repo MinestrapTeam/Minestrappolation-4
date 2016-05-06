19622aa93c64c71fead289db38e33d6e057cac02
Clashsoft <clashsoft@hotmail.com>
2016-05-06 17:08:00 +0200

    Update Workspace
    - Updated the Build Script.
    - Updated the `.gitignore` and `.gitattributes` files.
    - Removed the `*.launch` files.
    - Updated the Version Number to `1.8.9-4.3.0`.

--- src/main/java/minestrapteam/mods/minestrappolation/lib/MReference.java
+++ src/main/java/minestrapteam/mods/minestrappolation/lib/MReference.java
@@ -8,7 +8,7 @@ public class MReference
 	public static final String MODID   = "ministrapp";
 	public static final String ACRONYM = "M";
 	public static final String NAME    = "Minestrappolation Mod";
-	public static final String VERSION = "1.8.0-4.3.0";
+	public static final String VERSION = "1.8.9-4.3.0";
 
 	public static final List<String> AUTHORS = Arrays.asList("Delocuro", "TheWerty1124", "SoBioHazardous");
 
